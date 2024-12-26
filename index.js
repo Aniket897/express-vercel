e410fcb8358bab30f66baf6d892fad399dcdaa55
aniket-kangane <aniketkangane@pepsales.co>
2024-12-26 13:36:09 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, resp) => {
+  console.log("req comming");
+  resp.status(200).json({ message: "Hello World" });
+});
+
+app.listen(PORT, () => {
+  console.log("first server is running on port 8080");
+});
